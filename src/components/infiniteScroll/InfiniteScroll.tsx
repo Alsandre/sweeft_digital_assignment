b192610ea94305eddd2f14cb9fa21339dddfa863
alsandre <gabeditebuli@gmail.com>
2024-03-03 16:31:19 +0400

    feat: setup infinite scroll component

--- src/components/infiniteScroll/InfiniteScroll.tsx
+++ src/components/infiniteScroll/InfiniteScroll.tsx
@@ -1,10 +1,36 @@
+import { useRef, useState } from "react";
 import { useInfiniteScroll } from "../../hooks/useInfiniteScroll";
+import styles from "./infiniteScroll.module.css";
+
+const BOTTOM_SCROLL_OFFSET = 40;
+const BACK_TO_TOP_OFFSET = 300;
 
 export const InfiniteScroll: React.FC<{ term: string }> = ({ term }) => {
+  const containerRef = useRef<HTMLDivElement | null>(null);
+  const [isScrolled, setIsScrolled] = useState(false);
   const { scrollableData, setPageIndex, isFetching, error } =
     useInfiniteScroll(term);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handlScroll = (e: React.UIEvent<HTMLDivElement>) => {
+    setIsScrolled(e.currentTarget.scrollTop > BACK_TO_TOP_OFFSET);
+    !isFetching && setIsLoading(false);
+    if (isLoading) return;
+    const scrolledToBottom =
+      e.currentTarget.scrollHeight - BOTTOM_SCROLL_OFFSET <
+      e.currentTarget.scrollTop + e.currentTarget.clientHeight;
+    if (scrolledToBottom) {
+      setPageIndex((prevInd) => prevInd + 1);
+      setIsLoading(true);
+    }
+  };
+  const handleClick = () => {
+    if (containerRef?.current) {
+      containerRef.current.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+    }
+  };
   return (
-    <div>
+    <div ref={containerRef} onScroll={handlScroll}>
       {error && `<span>Error! message: ${error}!`}
       {scrollableData.length > 0 && (
         <ul>
@@ -18,6 +44,14 @@ export const InfiniteScroll: React.FC<{ term: string }> = ({ term }) => {
           {isFetching && "Loading..."}
         </ul>
       )}
+      <button
+        onClick={handleClick}
+        className={`${styles["back-to-top"]} ${
+          isScrolled && styles["fade-in"]
+        }`}
+      >
+        BACK TO TOP
+      </button>
     </div>
   );
 };
